52b7bb77ac3c6e19667e6adb5b9a36d5010f4b5d
Marcus <mvbios@gmail.com>
2014-06-11 02:36:35 -0300

    alteracao

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-wget -qO - https://deb.pkgr.io/key | sudo apt-key add -
-echo "deb https://deb.pkgr.io/gitlabhq/gitlabhq precise 6-9-stable" | sudo tee -a /etc/apt/sources.list.d/pkgr.list
-sudo apt-get update
-sudo apt-get install gitlab-ce
+wget https://raw.github.com/tripflex/igitlab/master/igitlab
+chmod +x igitlab
+./igitlab mygit.vvs
